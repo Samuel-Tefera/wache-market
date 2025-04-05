6c8106e3f754fffd864500ef96a7e29bb57a6ddb
Samuel-Tefera <samuelteferab@gmail.com>
2025-04-05 11:29:27 +0300

    add navigation and hero section for the page

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wache&dash;Market</title>
+    <link rel="stylesheet" href="assets/css/index.css" />
+</head>
+
+<body>
+    <header>
+        <nav class="navigation">
+            <a class="logo" href="#">wache market</a>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">How it Works</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="hero-section">
+            <div>
+                <h1 class="hero-title">Wachemo’s Trading Hub Awaits You</h1>
+                <p class="hero-text">Imagine a place where Wachemo students and staff come together to trade. <br />
+                    That’s
+                    us—a
+                    marketplace tailored
+                    for our
+                    university family. Sell your extra stuff, from books to bikes, or pick up what you need without
+                    leaving
+                    campus. It’s
+                    safe, local, and built with you in mind. Join today and connect with your community like never
+                    before!
+                </P>
+                <div class="hero-btns">
+                    <a class="btn main-btn" href="#">Start Shoping</a>
+                    <a class="btn" href="#">Explore More</a>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
