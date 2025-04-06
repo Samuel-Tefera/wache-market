587fe353cc2eea9adfea5a0a685c50397916dace
Samuel-Tefera <samuelteferab@gmail.com>
2025-04-06 18:28:00 +0300

    add navigation

---
+++ pages/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop Wachemo&dash;Browse Products</title>
+    <link rel="stylesheet" href="../assets/css/home.css" />
+</head>
+
+<body>
+    <header>
+        <nav class="navigation">
+            <a class="logo" href="#">wache market</a>
+            <form>
+                <input class="search-input" placeholder="Search by category" type="search">
+            </form>
+            <ul>
+                <li><a class="nav-btn" href="#">Home</a></li>
+                <li><a class="nav-btn" href="#">Profile</a></li>
+                <li><a class="nav-btn" href="#">Transaction</a></li>
+                <li><a class="nav-btn sign-up__btn" href="#">Logout</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
