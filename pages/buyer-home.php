e8e17e46ac0f96b92e3b1623327b3566d3a81ac5
Samuel-Tefera <samuelteferab@gmail.com>
2025-04-11 10:29:12 +0300

    split pages for buyer and seller

--- pages/buyer-home.php
+++ pages/buyer-home.php
@@ -19,12 +19,13 @@
                 <li><a class="nav-btn" href="#">Home</a></li>
                 <li><a class="nav-btn" href="#">Profile</a></li>
                 <li><a class="nav-btn" href="#">Transaction</a></li>
+                <li><a class="nav-btn" href="#">Cart</a></li>
                 <li><a class="nav-btn sign-up__btn" href="#">Logout</a></li>
             </ul>
         </nav>
     </header>
     <main>
-        <!-- <section class="category-section">
+        <section class="category-section">
             <div>
                 <p class="category-title">Books</p>
                 <div class="products">
@@ -173,37 +174,6 @@
                 </div>
             <div class="see-more">
                 <a class="see__more-btn btn" href="#">More Clothing</a>
-            </div>
-        </section> -->
-        <section class="category-section">
-            <div class="">
-                <p class="category-title">My Products</p>
-                <div class="products">
-                    <div class="product">
-                        <img class="product-img" alt="product image" src="../assets/images/samples/hacking.jpg">
-                        <div class="product-info">
-                            <p class="product-title">Hacking Multifactor Authentication</p>
-                            <p class="product-price">ETB<span> 230.00</span></p>
-                        </div>
-                    </div>
-                    <div class="product">
-                        <img class="product-img" alt="Running sports shoes" src="../assets/images/samples/running-shoe.jpg">
-                        <div class="product-info">
-                            <p class="product-title">Performance Running Shoes</p>
-                            <p class="product-price">ETB<span> 4200.00</span></p>
-                        </div>
-                    </div>
-                    <div class="product">
-                        <img class="product-img" alt="HP Pavilion G7 laptop" src="../assets/images/samples/laptop1.jpg">
-                        <div class="product-info">
-                            <p class="product-title">HP Pavilion G7 Laptop</p>
-                            <p class="product-price">ETB<span> 27500.00</span></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="see-more">
-                    <a class="see__more-btn btn" href="#">Add Product</a>
-                </div>
             </div>
         </section>
     </main>
---
+++ pages/seller-home.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop Wachemo&dash;Browse Products</title>
+    <link rel="stylesheet" href="../assets/css/home.css" />
+</head>
+
+<body>
+    <header>
+        <nav class="navigation">
+            <a class="logo" href="#">wache market</a>
+            <form>
+                <!-- <input class="search-input" placeholder="Search by category" type="search"> -->
+            </form>
+            <ul>
+                <li><a class="nav-btn" href="#">Home</a></li>
+                <li><a class="nav-btn" href="#">Profile</a></li>
+                <li><a class="nav-btn" href="#">Transaction</a></li>
+                <li><a class="nav-btn sign-up__btn" href="#">Logout</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="category-section">
+            <div class="">
+                <p class="category-title">My Products</p>
+                <div class="products">
+                    <div class="product">
+                        <img class="product-img" alt="product image" src="../assets/images/samples/hacking.jpg">
+                        <div class="product-info">
+                            <p class="product-title">Hacking Multifactor Authentication</p>
+                            <p class="product-price">ETB<span> 230.00</span></p>
+                        </div>
+                    </div>
+                    <div class="product">
+                        <img class="product-img" alt="Running sports shoes" src="../assets/images/samples/running-shoe.jpg">
+                        <div class="product-info">
+                            <p class="product-title">Performance Running Shoes</p>
+                            <p class="product-price">ETB<span> 4200.00</span></p>
+                        </div>
+                    </div>
+                    <div class="product">
+                        <img class="product-img" alt="HP Pavilion G7 laptop" src="../assets/images/samples/laptop1.jpg">
+                        <div class="product-info">
+                            <p class="product-title">HP Pavilion G7 Laptop</p>
+                            <p class="product-price">ETB<span> 27500.00</span></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="see-more">
+                    <a class="see__more-btn btn" href="#">Add Product</a>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
