bd3ebdaf9d92bdc376e7737b70802506857c32fd
linamellese <linamellese@gmail.com>
2025-04-15 11:56:28 +0300

    login and signup

---
+++ pages/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>login</title>
+    <link rel="stylesheet" href="../assets/css/login.css" />
+  </head>
+  <body>
+    <div class="container">
+      <form action="#" class="form" id="form2">
+        <h2 class="form__title">Log In</h2>
+        <input type="email" placeholder="Email" class="input" />
+        <input type="password" placeholder="Password" class="input" />
+        <button class="btn">Log In</button>
+      </form>
+    </div>
+  </body>
+</html>
