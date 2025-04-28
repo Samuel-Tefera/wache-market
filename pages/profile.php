52aa46eb57993ba9ff4000ceb7d104729eef928b
Samuel-Tefera <samuelteferab@gmail.com>
2025-04-28 15:09:49 +0300

    send request to fecth user profile and render profile data

--- pages/profile.php
+++ pages/profile.php
@@ -44,16 +44,17 @@ require_auth();
     <div class="container">
         <div class="profile-section">
             <div class="profile-card">
-                <img src="../assets/images/person-img/henock.jpg" alt="Profile Picture" class="avatar" />
+                <img id="user-image" src="../assets/images/person-img/henock.jpg" alt="Profile Picture" class="avatar" />
                 <div class="profile-info">
-                    <P>Name: <span>John Doe</span></P>
-                    <P>Email: <span>john@wache.com</span></P>
-                    <p>Address: Block <span>200</span>, Dorm # <span>108</span></p>
+                    <P>Name: <span id="user-name"></span></P>
+                    <P>Email: <span id="user-email"></span></P>
+                    <P>Phone: <span id="user-phone"></span></P>
+                    <p>Address: <span id="user-address"></span></p>
                 </div>
                 <div class="wallet-info">
                     <h3>Your Wallet</h3>
                     <div class="wallet-amount">
-                        <p>Amount: <span id="wallet-amount">1287.45</span></p>
+                        <p>Amount: <span id="wallet-amount"></span></p>
                         <button class="eye-toggle" onclick="toggleWalletVisibility()">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
                         </button>
@@ -62,14 +63,14 @@ require_auth();
                     <button class="btn" onclick="openPopup('withdraw')">Withdraw</button>
                 </div>
                 <div class="user-mode">
-                    <p>Current Mode: <span id="user-mode-display">Buyer</span></p>
+                    <p>Current Mode: <span id="user-mode-display"></span></p>
                     <div class="radio-group">
                         <div>
                             <label>Change mode here:</label>
                         </div>
                         <div class="radios">
                             <label>
-                                <input type="radio" name="userMode" value="buyer" checked
+                                <input type="radio" name="userMode" value="buyer"
                                     onchange="switchMode(this.value)">
                                 Buyer
                             </label>
@@ -85,32 +86,10 @@ require_auth();
         <div class="transaction-section">
             <h2>Recent Transactions</h2>
             <div class="transactions">
-                <div class="transaction positive">
-                    <p><strong>+ $200</strong> - Purchase - Telebirr</p>
-                    <p class="date">Apr 10, 2025</p>
-                </div>
-                <div class="transaction negative">
-                    <p><strong>- $150</strong> - Withdrawal - CBE</p>
-                    <p class="date">Apr 09, 2025</p>
-                </div>
-                <div class="transaction positive">
-                    <p><strong>+ $300</strong> - Sale - M-Pesa</p>
-                    <p class="date">Apr 08, 2025</p>
-                </div>
-                <div class="transaction negative">
-                    <p><strong>- $50</strong> - Purchase - Amole</p>
-                    <p class="date">Apr 07, 2025</p>
-                </div>
-                <div class="transaction negative">
-                    <p><strong>- $50</strong> - Purchase - Amole</p>
-                    <p class="date">Apr 07, 2025</p>
-                </div>
+
             </div>
         </div>
     </div>
-
-    <!-- Popup Modal -->
-    <!-- Withdraw/Deposit Popup -->
     <div class="popup" id="popup">
         <div class="popup-content">
             <h3 id="popup-title">Action</h3>
