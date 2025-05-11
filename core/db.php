f78531e8401c280832c01835e38f9ee68b430ec1
Samuel-Tefera <samuelteferab@gmail.com>
2025-05-11 11:32:22 +0300

    replace db info from env

--- core/db.php
+++ core/db.php
@@ -1,8 +1,11 @@
 <?php
-$host = 'localhost';
-$db   = 'wache-market';
-$user = 'root';
-$pass = '';
+require_once 'envloader.php';
+loadEnv(__DIR__ . '/../.env');
+
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 $conn = new mysqli($host, $user, $pass, $db);
 
